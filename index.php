339351aa21f8b8874db38dd2dc2fabc77e71c3a9
MikePio <michelepiopilla@gmail.com>
2023-05-16 00:24:12 +0200

    Created form in index.php

--- index.php
+++ index.php
@@ -1,21 +1,48 @@
 <?php 
 
-$testString = 'test';
+// $testString = 'test';
 
 ?>
 
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">  
   <title>PHP Badwords</title>
 </head>
-<body>
-  <h1>test</h1>
-  <?php echo $testString ?>
+<body class="bg-dark text-white">
+
+  <!-- <h1>test</h1> -->
+  <!-- <?php echo $testString ?> -->
+
+  <div class="container p-5">
+
+    <form>
+      <div class="mb-3">
+        <label for="emailForm" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+        <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+      </div>
+      <div class="mb-3">
+        <label for="passwordForm" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <!-- <div class="mb-3 form-check"> -->
+        <!-- <input type="checkbox" class="form-check-input" id="exampleCheck1"> -->
+        <!-- <label class="form-check-label" for="exampleCheck1">Check me out</label> -->
+      <!-- </div> -->
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+  </div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+
   
 </body>
 </html>
